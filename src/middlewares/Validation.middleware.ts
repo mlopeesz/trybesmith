512a7db72b1ccddbca8aa2fe2642862bface4d03
Mauricio Lopes <mlopeesz@gmail.com>
2022-06-22 17:54:45 -0300

    feat: user validation middleware

--- src/middlewares/Validation.middleware.ts
+++ src/middlewares/Validation.middleware.ts
@@ -1,9 +1,23 @@
 import { Request, Response, NextFunction } from 'express';
 import productSchema from '../schemas/product.schema';
+import userSchema from '../schemas/user.schema';
 
 class ValidationMiddleware {
   public validateProduct = async (req: Request, res: Response, next: NextFunction) => {
     const { error } = productSchema.validate(req.body);
+
+    if (error?.message.includes('is required')) {
+      return res.status(400).json({ message: error.message });
+    }
+
+    if (error?.message.includes('must be')) {
+      return res.status(422).json({ message: error.message });
+    }
+    next();
+  };
+
+  public validateUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { error } = userSchema.validate(req.body);
 
     if (error?.message.includes('is required')) {
       return res.status(400).json({ message: error.message });
