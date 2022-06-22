a40e774979af8cd8548a546e111dbb6d4b01fbb5
Mauricio Lopes <mlopeesz@gmail.com>
2022-06-22 15:27:52 -0300

    feat: adiciona rota post

--- src/routes/ProductRoutes.ts
+++ src/routes/ProductRoutes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import ProductController from '../controllers/ProductController';
+import ValidationMiddleware from '../middlewares/Validation.middleware';
 
 const productController = new ProductController();
+const validationMiddleware = new ValidationMiddleware();
 
 const router = Router();
 
 router.get('/', productController.getAll);
+router.post('/', validationMiddleware.validateProduct, productController.create);
 
 export default router;
