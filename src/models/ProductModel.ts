1becd9f0d02b9b2355a842c8aeda5393524664f0
Mauricio Lopes <mlopeesz@gmail.com>
2022-06-22 15:27:24 -0300

    feat: função create

--- src/models/ProductModel.ts
+++ src/models/ProductModel.ts
@@ -1,9 +1,24 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
 
 class ProductModel {
   public getAll = async () => {
-    const [products] = await connection.execute('SELECT * FROM Trybesmith.Products');
+    const [products] = await connection.execute(
+      'SELECT * FROM Trybesmith.Products',
+    );
     return products;
+  };
+
+  public create = async (name: string, amount: string, orderId: number | null = null) => {
+    const [product] = await connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount, orderId) VALUES(?,?,?)',
+      [name, amount, orderId],
+    );
+    return {
+      id: product.insertId,
+      name,
+      amount,
+    };
   };
 }
 
