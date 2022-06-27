2c15a80ba95400964c8795b14a0b2390ee5dcf8c
Mauricio Lopes <mlopeesz@gmail.com>
2022-06-27 11:03:25 -0300

    feat: Order routes / import em app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import productRoutes from './routes/ProductRoutes';
 import userRoutes from './routes/UserRoutes';
+import orderRoutes from './routes/OrderRoutes';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRoutes);
 app.use('/users', userRoutes);
+app.use('/orders', orderRoutes);
 
 export default app;
